e941a63af479726f6eb78b7ed8dc97eba1f04097
iaoiui <iaoiui0727@gmail.com>
2020-05-07 06:11:05 +0900

    mend script

--- 02-verify-etcd.sh
+++ 02-verify-etcd.sh
@@ -1,2 +1,2 @@
 CONTAINER_ID=$(docker ps | grep kind-control-plane$ | cut -c1-12)
-docker exec -it $CONTAINER_ID  /bin/sh -c  "sh verify-etcd.sh"
+docker exec -it $CONTAINER_ID  /bin/sh -c  "sh verify_etcd.sh"
